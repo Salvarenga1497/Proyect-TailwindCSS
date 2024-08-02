f696458c34ca2b55a12c2e94ea5c67f27caf15be
Victor Servin <sebaatian.alvarenga@gmail.com>
2024-08-02 15:47:58 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,70 @@
+const html = document.documentElement;
+const themeMenu = document.getElementById("theme-menu");
+const icons = {
+  light: document.getElementById("light-icon"),
+  dark: document.getElementById("dark-icon"),
+  system: document.getElementById("system-icon")
+}
+const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+const themeOptions = document.querySelectorAll(
+  "[data-theme-option]"
+);
+let currentTheme = localStorage.getItem("theme") || localStorage.setItem("theme", "system");
+
+
+updateTheme(currentTheme);
+updateThemeUI(currentTheme);
+
+function updateThemeUI(theme){
+  Object.entries(icons).forEach(([key, icon]) =>
+    key === theme
+      ? icon.classList.remove("hidden")
+      : icon.classList.add("hidden")
+  );
+
+  themeMenu.classList.add("hidden");
+  localStorage.setItem('theme', theme)
+}
+
+ function updateTheme(theme){
+    if (theme === 'dark' || (theme === "system" && isDarkMode.matches)) {
+      html.classList.add("dark");
+    } else if (theme === "light" || (theme === "system" && !isDarkMode.matches)) {
+      html.classList.remove("dark");
+    }
+
+    currentTheme = theme;
+};
+
+isDarkMode.addEventListener("change", ({ matches }) => {
+  if (currentTheme === "system") {
+    matches
+      ? html.classList.add("dark")
+      : html.classList.remove("dark");
+  }
+});
+
+themeOptions.forEach((option)=>{
+  option.addEventListener("click", () => {
+    const theme = option.dataset.themeOption
+
+    updateThemeUI(theme);
+    updateTheme(theme);
+  })
+})
+
+document.getElementById(
+  "toogle-theme-menu"
+).addEventListener('click', ()=>
+  themeMenu.classList.toggle("hidden")
+);
+
+const toggleMobileMenu = document.getElementById("toggle-mobile-menu");
+
+toggleMobileMenu.addEventListener('click', () =>{
+  document.getElementById("mobile-menu").classList.toggle("hidden");
+  document.getElementById("open-menu-icon").classList.toggle("hidden");
+  document.getElementById("close-menu-icon").classList.toggle("hidden");
+})
+
+
